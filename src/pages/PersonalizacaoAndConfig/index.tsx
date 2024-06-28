606641382335d3eb06bab19e8086992231bc71e1
ITAPEVI - GIUSEPPE ANGELO BERTOLLI PADRE <e921440w10@professor.educacao.sp.gov.br>
2024-06-28 08:39:24 -0300

    Criação da tela PersonalizacaoAndConfig

--- src/pages/PersonalizacaoAndConfig/index.tsx
+++ src/pages/PersonalizacaoAndConfig/index.tsx
@@ -1,9 +1,74 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, FlatList, Alert } from 'react-native'
+import { useNavigation } from "@react-navigation/native";
 
-export default function PagePersonalizacaoAndConfig(){
-    return(
-        <>
-        <Text style={{textAlign: "center"}}>PagePersonalizacaoAndConfig</Text>
-        </>
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Feather } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons'
+
+export default function PagePersonalizacaoAndConfig() {
+    const navigation = useNavigation();
+    const PageHome = () => {
+        navigation.goBack();
+    }
+
+    const ButtonAlert = () =>
+        Alert.alert('Alert Title', `Deseja mesmo sair do app?`, [
+            {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+            },
+            { text: 'OK', onPress: () => console.log('Deslogar') },
+        ]);
+
+    const sair = () => {
+        ButtonAlert();
+    };
+
+    return (
+        <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+            <TouchableOpacity onPress={PageHome} style={{ display: 'flex', flexDirection: 'row', marginLeft: 20, marginTop: 40, marginBottom: 60 }}>
+                <AntDesign name="left" size={20} color="#fff" />
+                <Text style={{ color: '#fff', fontSize: 15, marginLeft: 5 }}>Voltar</Text>
+            </TouchableOpacity>
+
+            <View style={styles.containerTwo}>
+                <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginLeft: 20, marginTop: 40 }}>
+                    <Entypo name='home' size={25} color={"#000"} />
+                    <Text style={{ fontWeight: "900", fontSize: 15, marginLeft: 10 }}>Inicio</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginLeft: 21, marginTop: 20 }}>
+                    <Octicons name="gear" size={25} color="black" />
+                    <Text style={{ fontWeight: "900", fontSize: 15, marginLeft: 10 }}>Configurações</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginLeft: 20, marginTop: 20 }}>
+                    <Feather name="help-circle" size={25} color="black" />
+                    <Text style={{ fontWeight: "900", fontSize: 15, marginLeft: 10 }}>Ajuda</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={sair} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginLeft: 20, marginTop: 20 }}>
+                    <Feather name="help-circle" size={25} color="black" />
+                    <Text style={{ fontWeight: "900", fontSize: 15, marginLeft: 10 }}>Sair</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    scrollViewContainer: {
+        justifyContent: 'flex-end',
+        backgroundColor: '#5D5C96', // Cor de fundo do ScrollView
+    },
+    containerTwo: {
+        flexGrow: 1,
+        backgroundColor: '#FFF',
+        flexDirection: 'column',
+        borderTopRightRadius: 40,
+        borderTopLeftRadius: 40,
+        height: 600
+    }
+});
